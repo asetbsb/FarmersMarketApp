0218b48ead114466ba70481f46ee5349c84a95f7
Asset <a11@MacBook-Air.local>
2024-11-20 00:34:48 +0500

    Updating

---
+++ FarmersMarketSystem/FarmerInterfaceControllers/RestockModalVC.swift
@@ -0,0 +1,29 @@
+//
+//  RestockModalVC.swift
+//  FarmersMarketSystem
+//
+//  Created by Asset on 11/19/24.
+//
+
+import UIKit
+
+class RestockModalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
